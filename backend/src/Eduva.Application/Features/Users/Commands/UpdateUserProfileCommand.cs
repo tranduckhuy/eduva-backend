16b245f4e67101f3028425d1537e78f36ba90bed
Trần Đức Huy <duchuy81.1610@gmail.com>
2025-07-17 12:06:21 +0700

    feat: Improves error logging and updates user profile
    Enhances error handling by including inner exception details
    in the API's base controller, aiding in debugging.
    
    Updates user profile command to allow null values for phone
    number and avatar URL, providing more flexibility in user data.

--- backend/src/Eduva.Application/Features/Users/Commands/UpdateUserProfileCommand.cs
+++ backend/src/Eduva.Application/Features/Users/Commands/UpdateUserProfileCommand.cs
@@ -10,7 +10,7 @@ namespace Eduva.Application.Features.Users.Commands
         public Guid UserId { get; set; }
 
         public string? FullName { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string AvatarUrl { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
+        public string? AvatarUrl { get; set; } = string.Empty;
     }
 }
