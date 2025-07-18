c36f0f7855d32cd6641e2b0ece49f53a2fb182b6
sangtran13 <tranngocsang147877@gmail.com>
2025-07-18 13:44:53 +0700

    feat(users): implement and fully test user export feature

---
+++ backend/src/Eduva.Application/Features/Users/Queries/ExportUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using Eduva.Application.Features.Users.Requests;
+using MediatR;
+
+namespace Eduva.Application.Features.Users.Queries
+{
+    public record ExportUsersQuery(ExportUsersRequest Request, Guid SchoolAdminId) : IRequest<byte[]>;
+}
