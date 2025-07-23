9a2b0dda12d8ba419027b958599c8979d99f6fa6
Trần Đức Huy <duchuy81.1610@gmail.com>
2025-07-23 11:33:05 +0700

    feat: delete ai job by jobId

---
+++ backend/src/Eduva.Application/Features/Jobs/Commands/DeleteCompletedJob/DeleteCompletedJobCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Eduva.Application.Features.Jobs.Commands.DeleteCompletedJob
+{
+    public class DeleteCompletedJobCommand : IRequest<Unit>
+    {
+        public Guid JobId { get; set; }
+        public bool Permanent { get; set; } = false;
+
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ backend/src/Eduva.Application/Features/Jobs/Commands/DeleteCompletedJob/DeleteCompletedJobHandler.cs
@@ -0,0 +1,50 @@
+﻿using Eduva.Application.Exceptions.Auth;
+using Eduva.Application.Exceptions.Job;
+using Eduva.Application.Interfaces;
+using Eduva.Application.Interfaces.Services;
+using Eduva.Domain.Enums;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Eduva.Application.Features.Jobs.Commands.DeleteCompletedJob
+{
+    public class DeleteCompletedJobHandler : IRequestHandler<DeleteCompletedJobCommand, Unit>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IStorageService _storageService;
+        private ILogger<DeleteCompletedJobHandler> _logger;
+
+        public DeleteCompletedJobHandler(IUnitOfWork unitOfWork, IStorageService storageService, ILogger<DeleteCompletedJobHandler> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _storageService = storageService;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteCompletedJobCommand request, CancellationToken cancellationToken)
+        {
+            var jobRepository = _unitOfWork.GetRepository<Domain.Entities.Job, Guid>();
+            var job = await jobRepository.GetByIdAsync(request.JobId)
+                ?? throw new JobNotFoundException();
+
+            if (job.UserId != request.UserId)
+            {
+                throw new ForbiddenException(["You are not authorized to delete this job because it does not belong to you."]);
+            }
+
+            if (request.Permanent)
+            {
+                jobRepository.Remove(job);
+                _logger.LogInformation("Deleting job {JobId} permanently.", job.Id);
+            }
+            else
+            {
+                job.Status = EntityStatus.Deleted;
+                jobRepository.Update(job);
+            }
+
+            await _unitOfWork.CommitAsync();
+            return Unit.Value;
+        }
+    }
+}
